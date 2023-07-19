646e6cf3ec6679b75cef5392c33ec9a8c7dc8671
stavgafny <stavgafny@gmail.com>
2023-07-20 00:08:38 +0300

    initializing flutter

---
+++ android/app/src/main/kotlin/com/example/coop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
